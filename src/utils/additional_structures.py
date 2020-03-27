ba654b793a179c95f592daae08e3b2a763ef9b48
Fenicheva Sofia <alicadr.032@gmail.com>
2020-03-27 19:26:41 +0300

    add search_by_prefix in WordTrie

--- src/utils/additional_structures.py
+++ src/utils/additional_structures.py
@@ -1,6 +1,3 @@
-import random
-
-
 class Word2vec:
     def get_word_vector(self, word):
         raise Exception('Not implemented')
@@ -11,7 +8,7 @@ class FastText(Word2vec):
         vector = [ 0 for i in range(300)]
         for i, ch in enumerate(word):
             vector[i] = ord(ch)
-        return vector
+        return vector        
 
 
 class _Node:
@@ -19,6 +16,27 @@ class _Node:
         self.char = char
         self.children = {}
         self.value = None
+        self.parent = None
+
+    def _get_prefix(self):
+        tmp_node = self
+        prefix = ''
+        while tmp_node.parent:
+            prefix = tmp_node.char + prefix
+            tmp_node = tmp_node.parent
+
+        return prefix
+
+    def get_childs(self):
+        stack = [(self, self._get_prefix())]
+
+        while stack:
+            tmp_node, prefix = stack.pop(0)
+            if tmp_node.value:
+                yield prefix, tmp_node.value
+            
+            for char, child in tmp_node.children.items():
+                stack.append((child, prefix+char))
 
 
 class WordTrie:
@@ -34,18 +52,27 @@ class WordTrie:
             if child is None:
                 child = _Node(char)
                 tmp_node.children[char] = child
+                child.parent = tmp_node
             tmp_node = child
-
 
         tmp_node.value = self.get_vector(word)
 
     def build_dict(self, words):
         for word in words:
             self.add(word)
+        return self
 
     def search_by_prefix(self, prefix):
-        pass
+        tmp_node = self.root
+
+        for char in prefix:
+            tmp_node = tmp_node.children.get(char, None)
+            if tmp_node is None:
+                return
+        yield from tmp_node.get_childs()
 
 
 # wt = WordTrie(FastText())
 # wt.build_dict(['word', 'world', 'cat', 'cats'])
+# for word, vector in wt.search_by_prefix('cats'): 
+#     print(word)

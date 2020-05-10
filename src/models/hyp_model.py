b338bb1010497652ddef467b0f8086e2874d6175
Fenicheva Sofia <alicadr.032@gmail.com>
2020-05-10 14:50:39 +0300

    fix bugs HypWords

--- src/models/hyp_model.py
+++ src/models/hyp_model.py
@@ -4,34 +4,18 @@ from src.utils.features_sentence import search_simple_bigramm
 from src.utils.dictionary import text2LemmsModel, get_prefix_trie
 
 
-def get_hyponym_and_hypernym(wikiwordnet, word):
-    synsets = wikiwordnet.get_synsets(word)
-    if not synsets:
-        return {}
-
-    synset1 = synsets[0]
-    set_words = set()
-
-    for hyponym in wikiwordnet.get_hyponyms(synset1):
-        set_words |= { w.lemma() for w in hyponym.get_words()}
-
-    for hypernym in wikiwordnet.get_hypernyms(synset1):
-        set_words |= { w.lemma() for w in hypernym.get_words()}
-
-    return set_words
-
-
 class HypWords:
     # hyponym and hypernym words
     def __init__(self, prefix_trie=None):
         self.wikiwordnet = WikiWordnet()
         self.prefix_trie = get_prefix_trie() # перенести потом в бота, инициализировать перед запуском, как и остальные модели
 
-    def get_hyp_with_prefix(self, word, words_with_prefix):
+    def _get_hyp_with_prefix(self, word, words_with_prefix):
         hyp_w = get_hyponym_and_hypernym(self.wikiwordnet, word)
         answer = hyp_w & words_with_prefix
         if answer:
             return list(answer)
+        return []
 
     def get_words(self, sentence, prefix):
         list_lex = text2LemmsModel.get_lemms(sentence)
@@ -41,7 +25,7 @@ class HypWords:
 
         if bigramm_w:
             bigramm = ' '.join(bigramm_w)
-            ans = self.get_hyp_with_prefix(bigramm, words_with_prefix)
+            ans = self._get_hyp_with_prefix(bigramm, words_with_prefix)
             if ans:
                 return ans
             word = bigramm_w[1]
@@ -51,4 +35,22 @@ class HypWords:
                     word = w['lex']
                     break
 
-        return self.get_hyp_with_prefix(word, words_with_prefix)
+        return self._get_hyp_with_prefix(word, words_with_prefix)
+
+
+def get_hyponym_and_hypernym(wikiwordnet, word):
+    synsets = wikiwordnet.get_synsets(word)
+    set_words = set()
+
+    if not synsets:
+        return set_words
+
+    synset1 = synsets[0]
+
+    for hyponym in wikiwordnet.get_hyponyms(synset1):
+        set_words |= { w.lemma() for w in hyponym.get_words()}
+
+    for hypernym in wikiwordnet.get_hypernyms(synset1):
+        set_words |= { w.lemma() for w in hypernym.get_words()}
+
+    return set_words

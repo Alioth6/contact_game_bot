ebb13bf74ebff7396492d8dd8a9c8876b4e949b4
Alioth6 <fomalhaut.hm@gmail.com>
2020-05-15 06:19:22 +0300

    Error checking in search_simple_bigramm

--- src/utils/features_sentence.py
+++ src/utils/features_sentence.py
@@ -10,7 +10,21 @@ def search_simple_bigramm(lemma_list):
 
     for w1, w2 in zip(lemma_list, lemma_list[1:]):
         if w1['pos']=='A' and w2['pos']=='S':
-            w2 = w2['lex']
-            gender = morph.parse(w2)[0].tag.gender 
-            w1 = morph.parse(w1['lex'])[0].inflect({gender})
-            return w1.word, w2
+            # parsing word - receiving list of possible values
+            w1_vars = morph.parse(w1['lex'])
+            # choosing elements with compatible POS
+            w1_vars = [i for i in w1_vars if i.tag.POS == 'ADJF']
+
+            word2 = w2['lex']
+            w2_vars = morph.parse(word2)
+            w2_vars = [i for i in w2_vars if i.tag.POS == 'NOUN']
+
+            if len(w1_vars) == 0 or len(w2_vars) == 0:
+                return
+            gender = w2_vars[0].tag.gender
+
+            try:
+                word1 = w1_vars[0].inflect({gender}).word
+            except ValueError:
+                word1 = w1_vars[0].word
+            return word1, word2

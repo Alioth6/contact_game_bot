0d8d893ff2ca35f9ac6f3265290049e30b9151bc
Alioth6 <fomalhaut.hm@gmail.com>
2020-10-12 14:57:00 +0300

    Sum model with compressed fasttext

--- src/bot/settings.py
+++ src/bot/settings.py
@@ -1,5 +1,6 @@
 import urllib.request
 import os.path
+import compress_fasttext
 from enum import Enum
 # from src.models.hyp_model import HypWords
 # from src.models.sim_model import SimWords
@@ -26,24 +27,16 @@ def init_no_models():
 
 
 def init_models():
-    url = 'http://panchenko.me/data/dsl-backup/w2v-ru/all.norm-sz100-w10-cb0-it1-min100.w2v'
-    rdt_name = 'data/RDT_light.w2v'
-    if not os.path.isfile(rdt_name):
-        print('Beginning w2v file download...')
-        urllib.request.urlretrieve(url, rdt_name)
-
-    print('Loading W2v...')
-    mod_rdt = KeyedVectors.load_word2vec_format(rdt_name, binary=True, unicode_errors='ignore')
-
     print('Models initialization...')
 
-    # hypWords = HypWords()
-    sum_words = SumWords(mod_rdt, 20)
+    fasttext_mod_url = 'https://github.com/avidale/compress-fasttext/releases/download/v0.0.1/ft_freqprune_400K_100K_pq_300.bin'
+    fasttext_mod = compress_fasttext.models.CompressedFastTextKeyedVectors.load(fasttext_mod_url)
+
+    sum_words = SumWords(fasttext_mod, 20)
 
     print('Ready')
 
     return [
-        # hypWords,
         sum_words
     ]
 
@@ -60,5 +53,5 @@ def init_puzzle_nouns():
     return puzzle_nouns
 
 
-LIST_MODELS = init_no_models()
+LIST_MODELS = init_models()
 LIST_PUZZLE_NOUNS = init_puzzle_nouns()
